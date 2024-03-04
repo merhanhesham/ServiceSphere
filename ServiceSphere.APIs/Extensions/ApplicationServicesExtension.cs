00971cbef39d9fc35d804162f1ef34829bec7574
merhanhesham <merhan100.hesham@gmail.com>
2024-03-05 00:00:22 +0200

    SS

---
+++ ServiceSphere.APIs/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,27 @@
+﻿using ServiceSphere.core.Repositeries.contract;
+using ServiceSphere.repositery.Data;
+using ServiceSphere.repositery;
+using ServiceSphere.APIs.Helper;
+using ServiceSphere.core.Entities.Services;
+using ServiceSphere.core.Services.contract;
+using ServiceSphere.services;
+
+namespace ServiceSphere.APIs.Extensions
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services) 
+        {
+            //allow mapping
+            services.AddAutoMapper(typeof(MappingProfile));
+            services.AddScoped(typeof(IGenericRepositery<>), typeof(GenericRepositery<>));
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+            services.AddScoped(typeof(INotificationService), typeof(NotificationService));
+
+            return services;
+        }
+    }
+}
